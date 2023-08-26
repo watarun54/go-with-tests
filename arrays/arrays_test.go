40c59cccade6dc49e2edebe49c9566c20bf54ec9
watarun54 <watarun54@gmail.com>
2023-08-26 21:54:53 +0900

    add SumAll

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -1,6 +1,9 @@
 package arrays
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of 5 numbers", func(t *testing.T) {
@@ -13,4 +16,13 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d expected %d given, %v", got, expected, numbers)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	expected := []int{3, 9}
+
+	if !reflect.DeepEqual(got, expected) {
+		t.Errorf("got %v expected %v", got, expected)
+	}
 }
