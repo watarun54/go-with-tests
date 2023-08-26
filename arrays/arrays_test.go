58c4989074dc3f62d18965e08d544222fb861b6c
watarun54 <watarun54@gmail.com>
2023-08-26 22:13:00 +0900

    add func checkSums as var func

--- arrays/arrays_test.go
+++ arrays/arrays_test.go
@@ -28,21 +28,22 @@ func TestSumAll(t *testing.T) {
 }
 
 func TestSumAllTails(t *testing.T) {
-	t.Run("make the sums of some slices", func(t *testing.T) {
-		got := SumAllTails([]int{1, 2}, []int{0, 9})
-		expected := []int{2, 9}
 
+	checkSums := func(t testing.TB, got, expected []int) {
+		t.Helper()
 		if !reflect.DeepEqual(got, expected) {
 			t.Errorf("got %v expected %v", got, expected)
 		}
+	}
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		expected := []int{2, 9}
+		checkSums(t, got, expected)
 	})
 
 	t.Run("safely sum empty slices", func(t *testing.T) {
 		got := SumAllTails([]int{}, []int{3, 4, 5})
 		expected := []int{0, 9}
-
-		if !reflect.DeepEqual(got, expected) {
-			t.Errorf("got %v expected %v", got, expected)
-		}
+		checkSums(t, got, expected)
 	})
 }
