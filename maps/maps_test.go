2fab4b97782f948a107120a8241c058e263b3b2d
watarun54 <watarun54@gmail.com>
2023-08-27 18:13:29 +0900

    add: Add method

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -15,8 +15,25 @@ func TestSearch(t *testing.T) {
 	t.Run("unknwon word", func(t *testing.T) {
 		_, err := dictionary.Search("unknown")
 
+		if err == nil {
+			t.Fatal("expected to get an error.")
+		}
+
 		assertError(t, err, ErrNotFound)
 	})
+}
+
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
+
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertStrings(t, got, want)
 }
 
 func assertStrings(t testing.TB, got, want string) {
@@ -29,10 +46,6 @@ func assertStrings(t testing.TB, got, want string) {
 
 func assertError(t testing.TB, got, want error) {
 	t.Helper()
-
-	if got == nil {
-		t.Fatal("expected to get an error.")
-	}
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
