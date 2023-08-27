928236e0bb16a8ef18bdd46205b6d15ceec6594e
watarun54 <watarun54@gmail.com>
2023-08-27 18:18:39 +0900

    refactor: assertDefinition

--- maps/maps_test.go
+++ maps/maps_test.go
@@ -25,15 +25,12 @@ func TestSearch(t *testing.T) {
 
 func TestAdd(t *testing.T) {
 	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
+	word := "test"
+	definition := "this is just a test"
 
-	want := "this is just a test"
-	got, err := dictionary.Search("test")
-	if err != nil {
-		t.Fatal("should find added word:", err)
-	}
+	dictionary.Add(word, definition)
 
-	assertStrings(t, got, want)
+	assertDefinition(t, dictionary, word, definition)
 }
 
 func assertStrings(t testing.TB, got, want string) {
@@ -50,4 +47,15 @@ func assertError(t testing.TB, got, want error) {
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string) {
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
+
+	assertStrings(t, got, definition)
 }
