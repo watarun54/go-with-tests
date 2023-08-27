0b089adc0be118f27f00e61ecfda607e4f4a4f46
watarun54 <watarun54@gmail.com>
2023-08-27 10:51:28 +0900

    refactor: name the fields

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -22,9 +22,9 @@ func TestArea(t *testing.T) {
 		shape    Shape
 		expected float64
 	}{
-		{Rectangle{12, 6}, 72.0},
-		{Circle{10}, 314.1592653589793},
-		{Triangle{12, 6}, 36},
+		{shape: Rectangle{Width: 12, Height: 6}, expected: 72.0},
+		{shape: Circle{Radius: 10}, expected: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, expected: 36},
 	}
 
 	for _, tt := range areaTests {
