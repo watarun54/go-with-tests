6597e8fdd4209ec52d73142a8050b2d778ddc621
watarun54 <watarun54@gmail.com>
2023-08-27 11:10:28 +0900

    refactor: change func into method of Perimeter

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPerimeter(t *testing.T) {
 	rectangle := Rectangle{10.0, 10.0}
-	got := Perimeter(rectangle)
+	got := rectangle.Perimeter()
 	expected := 40.0
 
 	if got != expected {
