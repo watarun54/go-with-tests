c4a7ba353b5b5a5c003565ad338282f0da6b2141
watarun54 <watarun54@gmail.com>
2023-09-14 23:54:36 +0900

    test function

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -29,6 +29,11 @@ func Walk(x interface{}, fn func(input string)) {
 		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
 			walkValue(v)
 		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walkValue(res)
+		}
 	}
 }
 
